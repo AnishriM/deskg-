8b91f19e81d3f76ddab2f7a27fba4cd4cc94b071
Sagar Mhantati <34499571+AnishriM@users.noreply.github.com>
2024-06-12 00:46:03 +0530

    Create Duck.java

---
+++ Duck.java
@@ -0,0 +1,16 @@
+package stratergy_pattern;
+
+public class Duck {
+	
+	public void display() {
+		System.out.println("I'm Duck.");
+	}
+	
+	public void fly() {
+		System.out.println("Fly...");
+	}
+	
+	public void quack() {
+		System.out.println("Quack!!!");
+	}
+}
